1cd9a69833c6bf6d1e4298ad029392cde8cc3ebc
mafuyan <mafuyan@hnu.edu.cn>
2021-06-07 09:05:58 +0800

    first

--- exception.py
+++ exception.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 # -*- encoding=utf8 -*-
-from log import logger
-
 
 class AsstException(Exception):
 
     def __init__(self, message):
         super().__init__(message)
-        logger.error(message)
+        print(message)
